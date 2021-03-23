5995dd4a50c1cfc21e3c828dbcf1e79d229e9a7d
Andreas Björn <coriakin@users.noreply.github.com>
2021-03-23 11:20:21 +0100

    If arranging for notification in different way one does not need to be warned about pushover not being set up

--- index.mjs
+++ index.mjs
@@ -4,9 +4,5 @@ import { hasSetupPushover } from './io/pushover.mjs'
 import logger from './io/log.mjs'
 
 (async () => {
-  if (!hasSetupPushover()) {
-    logger.warn(`Pushover not setup -- won't send any push notifications`)
-  }
-
   await openSupplyDrop(await getAuthCookie())
 })()
